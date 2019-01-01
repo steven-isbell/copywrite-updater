0a05a9b1dd89645dbef4b46608d93f7b49ab2b66
Steven Isbell <steveni_92@msn.com>
2019-01-01 11:38:15 -0700

    test

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const sleep = require('./sleep');
 
 const noLoginInfoErr = 'Please provide a username or email and password';
 
-(async () => {
+const { REPOSITORY_PAGE: rp } = process.env(async () => {
+  let count = 1;
   if (!process.argv[2] && !process.argv[3]) {
     throw new Error(noLoginInfoErr);
   }
@@ -17,5 +18,14 @@ const noLoginInfoErr = 'Please provide a username or email and password';
   await page.keyboard.type(process.argv[3]);
   await page.click('.btn.btn-primary.btn-block');
   await page.waitForNavigation();
-  await page.goto(process.env.REPOSITORY_PAGE);
+  await page.goto(rp);
+  await sleep(page, 60000);
+  //   while (!page.$('.blankslate')) {
+  const repos = page.$('.org-repos');
+  for (let i = 0; i < repos.length; i++) {
+    console.log(repos[i]);
+  }
+  count += 1;
+  // await page.goto(`${rp}?page={count}`);
+  //   }
 })();
